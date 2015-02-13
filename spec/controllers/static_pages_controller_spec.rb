63017e742886d70a1b6d81b419d53fec4c93499b
Bijal Patel <bijalpatel@hotmail.com>
2015-02-13 13:35:46 -0800

    Added contact page

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -4,25 +4,35 @@ require 'spec_helper'
 include RSpecHtmlMatchers
 
 RSpec.describe StaticPagesController, :type => :controller do
+
   render_views
+  before :each do
+    @base_title = "Bijal's App with Ruby on Rails"
+  end
 
   it "should get home" do
     get :home
     expect(response).to have_http_status(200)
-    expect(response.body).to have_tag('title', :text =>"Home | Bijal's App with Ruby on Rails")
+    expect(response.body).to have_tag('title', :text =>"Home | #{@base_title}")
   end
 
   it "should get help" do
     get :help
     expect(response).to have_http_status(200)
-    expect(response.body).to have_tag('title', :text =>"Help | Bijal's App with Ruby on Rails")
+    expect(response.body).to have_tag('title', :text =>"Help | #{@base_title}")
   end
 
   it "should get about" do
     get :about
     expect(response).to have_http_status(200)
-    expect(response.body).to have_tag('title', :text =>"About | Bijal's App with Ruby on Rails")
+    expect(response.body).to have_tag('title', :text =>"About | #{@base_title}")
   end
   
+  it "should get contact" do
+    get :contact
+    expect(response).to have_http_status(200)
+    expect(response.body).to have_tag('title', :text =>"Contact | #{@base_title}")
+  end
+
 end
 
