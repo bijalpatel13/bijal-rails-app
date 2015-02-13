043da306630b9fd6cbaadc3397a67b1e39891475
Bijal Patel <bijalpatel@hotmail.com>
2015-02-12 17:03:27 -0800

    Added rspec test files

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
   gem 'sqlite3',     '1.3.9'
+  gem 'rspec-rails'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  resources :microposts
+  get 'static_pages/about'
 
   resources :users
   root 'users#index'
