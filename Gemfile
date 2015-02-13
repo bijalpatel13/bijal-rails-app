aa8ba458bc7a1312c277af8d293a8ab0a73da5d6
Bijal Patel <bijalpatel@hotmail.com>
2015-02-13 12:51:34 -0800

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -14,13 +14,14 @@ group :development, :test do
   gem 'web-console', '2.0.0.beta3'
   gem 'spring',      '1.1.3'
   gem 'sqlite3',     '1.3.9'
-  gem 'rspec-rails'
+  gem 'rspec-rails', '3.1.0'
 end
 
 group :test do
-  gem 'minitest-reporters', '1.0.5'
-  gem 'mini_backtrace',     '0.1.3'
-  gem 'guard-minitest',     '2.3.1'
+  gem 'minitest-reporters',  '1.0.5'
+  gem 'mini_backtrace',      '0.1.3'
+  gem 'guard-minitest',      '2.3.1'
+  gem 'rspec-html-matchers', '0.7.0'
 end
 
 group :production do
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
+require 'spec_helper'
+
+include RSpecHtmlMatchers
 
 RSpec.describe StaticPagesController, :type => :controller do
+  render_views
 
   it "should get home" do
     get :home
     expect(response).to have_http_status(200)
+    expect(response.body).to have_tag('title', :text =>"Home | Bijal's App with Ruby on Rails")
   end
 
   it "should get help" do
     get :help
     expect(response).to have_http_status(200)
+    expect(response.body).to have_tag('title', :text =>"Help | Bijal's App with Ruby on Rails")
   end
 
   it "should get about" do
     get :about
     expect(response).to have_http_status(200)
+    expect(response.body).to have_tag('title', :text =>"About | Bijal's App with Ruby on Rails")
   end
   
 end
